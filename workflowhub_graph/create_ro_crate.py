f2bab54c7ee45912585738fc8829505198512cee
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2024-08-14 12:18:06 +0100

    create_ro_crate.py now builds an RO crate that better fits workflow run profile (#39)
    - Addresses #39
    - create_ro_crate.py refactored and updated to create crate that includes CreateAction with relevant metadata
    - Metadata added to Dockerfile
    - Example RO Crate Metadata updated to reflect this

--- workflowhub_graph/create_ro_crate.py
+++ workflowhub_graph/create_ro_crate.py
@@ -1,6 +1,8 @@
 import sys
+import uuid
+from datetime import datetime
 
-from rocrate.model import Person
+from rocrate.model import ContextEntity, Person
 from rocrate.rocrate import ROCrate
 
 
@@ -14,38 +16,13 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
     """
     crate = ROCrate()
 
-    data_entity = crate.add_file(
-        input_file,
-        properties={
-            "name": "Merged Data File",
-            "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub.",
-            "encodingFormat": "text/turtle",
-        },
+    crate.name = "WorkflowHub Knowledge Graph"
+    crate.description = (
+        "This RO-Crate contains the merged RDF triples from multiple RO-Crates sourced from "
+        "WorkflowHub."
     )
 
-    crate.add_dataset(
-        "./workflowhub_graph/",
-        properties={
-            "name": "WorkflowHub Graph",
-            "description": "A directory containing modules used by the workflow.",
-        },
-    )
-
-    crate.add_file("./Dockerfile")
-    crate.add_file("./poetry.lock")
-    crate.add_file("./README.md")
-
-    workflow_entity = crate.add_workflow(
-        source=workflow_file,
-        properties={
-            "name": "Snakemake Workflow",
-            "description": "This is the Snakemake workflow used to generate the merged RDF triples.",
-        },
-        main=True,
-        lang="snakemake",
-    )
-
-    # Authors:
+    # Add authors:
     alice = crate.add(
         Person(
             crate,
@@ -61,13 +38,72 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
         )
     )
 
-    crate.name = "WorkflowHub Knowledge Graph"
-    crate.description = "Test Description"
+    # Add dataset and files:
+    crate.add_dataset(
+        "./workflowhub_graph/",
+        properties={
+            "name": "WorkflowHub Graph",
+            "description": "A directory containing modules used by the workflow.",
+        },
+    )
 
-    data_entity["author"] = [alice, bob]
+    crate.add_file(
+        "./Dockerfile",
+        properties={
+            "@type": "File",
+            "name": "Dockerfile",
+            "encodingFormat": "application/yaml",
+            "description": "The Dockerfile used to build the Docker images for the workflow.",
+            "conformsTo": {"@id": "https://docs.docker.com/reference/dockerfile/"},
+        },
+    )
 
-    workflow_entity["output"] = data_entity
-    workflow_entity["author"] = [alice, bob]
+    crate.add_file("./poetry.lock")
+    crate.add_file("./README.md")
+
+    # Add data and workflow entities:
+    data_entity = crate.add_file(
+        input_file,
+        properties={
+            "name": "Merged Data File",
+            "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub.",
+            "encodingFormat": "text/turtle",
+            "author": [alice["@id"], bob["@id"]],
+        },
+    )
+
+    workflow_entity = crate.add_workflow(
+        source=workflow_file,
+        properties={
+            "name": "Snakemake Workflow",
+            "description": "This is the Snakemake workflow used to generate the merged RDF triples.",
+            "author": [alice["@id"], bob["@id"]],
+            "output": data_entity["@id"],
+        },
+        main=True,
+        lang="snakemake",
+    )
+
+    if "conformsTo" not in crate.root_dataset:
+        crate.root_dataset.append_to(
+            "conformsTo", {"@id": "https://w3id.org/ro/wfrun/workflow/0.5"}
+        )
+
+    crate.add(
+        ContextEntity(
+            crate,
+            identifier=str(uuid.uuid4()),
+            properties={
+                "@type": "CreateAction",
+                "name": "Merge RDF Triples",
+                "description": "Merging RDF triples from sourced crates.",
+                "agent": [alice["@id"], bob["@id"]],
+                "endTime": datetime.now().time().isoformat(),
+                "instrument": workflow_entity["@id"],
+                "result": data_entity["@id"],
+            },
+        )
+    )
 
     # Writing the RO-Crate metadata:
     crate.write(output_dir)
