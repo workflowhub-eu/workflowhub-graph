8db4f9caf33d2be1d7e4aa1da17c08f19c5fafc8
Alex Hambley <33315205+alexhambley@users.noreply.github.com>
2024-08-21 14:57:16 +0100

    Update authors and readme.md
    - Addresses #40, #39, #38, #36
    - Added newer diagram to readme.
    - Removed references to ghcr, this will be added back later
    - Added proper authors and orcids
    - Replaced Dockerfile encodingFormat with text/plain
    - Added created_files.json to RO Crate and added to data_entity["isBasedOn"].

--- workflowhub_graph/create_ro_crate.py
+++ workflowhub_graph/create_ro_crate.py
@@ -12,7 +12,6 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
     :param input_file: The input file provided by the Snakemake workflow (e.g., merged data file).
     :param workflow_file: Reference to the Snakemake workflow.
     :param output_dir: The output directory to store the RO-Crate metadata file.
-    :return:
     """
     crate = ROCrate()
 
@@ -23,18 +22,54 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
     )
 
     # Add authors:
-    alice = crate.add(
+    auth_1 = crate.add(
         Person(
             crate,
             "https://orcid.org/0000-0000-0000-0000",
-            properties={"name": "Alice Doe", "affiliation": "University of Flatland"},
+            properties={
+                "name": "Alexander Hambley",
+                "affiliation": "University of Manchester",
+            },
         )
     )
-    bob = crate.add(
+    auth_2 = crate.add(
         Person(
             crate,
-            "https://orcid.org/0000-0000-0000-0001",
-            properties={"name": "Bob Doe", "affiliation": "University of Flatland"},
+            "https://orcid.org/0000-0002-0035-6475",
+            properties={
+                "name": "Eli Chadwick",
+                "affiliation": "University of Manchester",
+            },
+        )
+    )
+    auth_3 = crate.add(
+        Person(
+            crate,
+            "https://orcid.org/0000-0002-4565-9760",
+            properties={
+                "name": "Oliver Woolland",
+                "affiliation": "University of Manchester",
+            },
+        )
+    )
+    auth_4 = crate.add(
+        Person(
+            crate,
+            "https://orcid.org/0000-0001-9842-9718",
+            properties={
+                "name": "Stian Soiland-Reyes",
+                "affiliation": "University of Manchester",
+            },
+        )
+    )
+    auth_5 = crate.add(
+        Person(
+            crate,
+            "https://orcid.org/0000-0001-6353-0808",
+            properties={
+                "name": "Volodymyr Savchenko",
+                "affiliation": "University of Geneva",
+            },
         )
     )
 
@@ -52,8 +87,19 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
         properties={
             "@type": "File",
             "name": "Dockerfile",
-            "encodingFormat": "application/yaml",
+            "encodingFormat": "text/plain",
             "description": "The Dockerfile used to build the Docker images for the workflow.",
+            "conformsTo": {"@id": "https://docs.docker.com/reference/dockerfile/"},
+        },
+    )
+
+    created_files = crate.add_file(
+        "./created_files.json",
+        properties={
+            "@type": "File",
+            "name": "created_files.json",
+            "encodingFormat": "application/json",
+            "description": "A JSON file containing the list of files sourced by the workflow.",
             "conformsTo": {"@id": "https://docs.docker.com/reference/dockerfile/"},
         },
     )
@@ -68,42 +114,32 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
             "name": "Merged Data File",
             "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub.",
             "encodingFormat": "text/turtle",
-            "author": [alice["@id"], bob["@id"]],
         },
     )
+
+    data_entity["author"] = [auth_1, auth_2, auth_3, auth_4, auth_5]
+    data_entity["isBasedOn"] = created_files
 
     workflow_entity = crate.add_workflow(
         source=workflow_file,
         properties={
             "name": "Snakemake Workflow",
             "description": "This is the Snakemake workflow used to generate the merged RDF triples.",
-            "author": [alice["@id"], bob["@id"]],
-            "output": data_entity["@id"],
         },
         main=True,
         lang="snakemake",
     )
+
+    workflow_entity["author"] = [auth_1, auth_2, auth_3, auth_4]
+    workflow_entity["output"] = data_entity
 
     if "conformsTo" not in crate.root_dataset:
         crate.root_dataset.append_to(
             "conformsTo", {"@id": "https://w3id.org/ro/wfrun/workflow/0.5"}
         )
 
-    crate.add(
-        ContextEntity(
-            crate,
-            identifier=str(uuid.uuid4()),
-            properties={
-                "@type": "CreateAction",
-                "name": "Merge RDF Triples",
-                "description": "Merging RDF triples from sourced crates.",
-                "agent": [alice["@id"], bob["@id"]],
-                "endTime": datetime.now().time().isoformat(),
-                "instrument": workflow_entity["@id"],
-                "result": data_entity["@id"],
-            },
-        )
-    )
+    # Add license:
+    crate.license = "https://opensource.org/license/bsd-2-clause"
 
     # Writing the RO-Crate metadata:
     crate.write(output_dir)
