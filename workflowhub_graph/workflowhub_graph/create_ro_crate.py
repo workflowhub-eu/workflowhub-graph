1d9dbe57a21e4acbca089bf76683abfde3cd225c
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2025-08-13 13:00:03 +0100

    include all outputs; set up FormalParameter entities; apply best practices

--- workflowhub_graph/workflowhub_graph/create_ro_crate.py
+++ workflowhub_graph/workflowhub_graph/create_ro_crate.py
@@ -9,10 +9,18 @@ from rocrate.model import ContextEntity, Person
 from rocrate.rocrate import ROCrate
 
 
-def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> None:
+def create_ro_crate(
+    full_graph_file: str,
+    base_graph_file: str,
+    enrichments_dir: str,
+    workflow_file: str,
+    output_dir: str,
+) -> None:
     """
     Create an RO-Crate metadata file for the given input file and output directory.
-    :param input_file: The input file provided by the Snakemake workflow (e.g., merged data file).
+    :param full_graph_file: The full, enriched knowledge graph.
+    :param base_graph_file: The base knowledge graph prior to enrichment.
+    :param enrichments_dir: Directory containing individual enrichments.
     :param workflow_file: Reference to the Snakemake workflow.
     :param output_dir: The output directory to store the RO-Crate metadata file.
     """
@@ -20,8 +28,10 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
 
     crate.name = "WorkflowHub Knowledge Graph"
     crate.description = (
-        "This RO-Crate contains the merged RDF triples from multiple RO-Crates sourced from "
-        "WorkflowHub."
+        "This RO-Crate contains a knowledge graph built from RO-Crates "
+        "on WorkflowHub, enriched with metadata from additional sources. "
+        "The source code used to create the knowledge graph, including the enrichments, "
+        "is also included."
     )
 
     org_uniman = crate.add(
@@ -118,7 +128,8 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
     crate.add_dataset(
         "/app/workflowhub_graph/",
         properties={
-            "name": "WorkflowHub Graph",
+            "@type": ["Dataset", "SoftwareSourceCode"],
+            "name": "WorkflowHub Graph source code",
             "description": "A directory containing modules used by the workflow.",
         },
     )
@@ -136,24 +147,132 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
 
     crate.add_file("/app/README.md")
 
-    # Add data and workflow entities:
-    data_entity = crate.add_file(
-        input_file,
+    config_file = crate.add_file(
+        "/app/config.yaml",
         properties={
-            "name": "Merged Data File",
-            "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub.",
+            "name": "Workflow configuration",
+            "description": "YAML file containing the configuration of parameters used for the workflow execution.",
+            "encodingFormat": "application/yaml",
+        },
+    )
+
+    # Add data and workflow entities:
+    full_graph_entity = crate.add_file(
+        full_graph_file,
+        properties={
+            "name": "Enriched WorkflowHub Knowledge Graph",
+            "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub, enriched with metadata from related sources.",
             "encodingFormat": "text/turtle",
         },
     )
 
-    data_entity["author"] = [
+    full_graph_entity["author"] = [
         auth_alex,
         auth_eli,
         auth_oliver,
         auth_stian,
         auth_volodymyr,
     ]
-    # data_entity["isBasedOn"] = created_files
+
+    # Add data and workflow entities:
+    enrichment_files = crate.add_dataset(
+        enrichments_dir,
+        properties={
+            "name": "Graph enrichment files",
+            "description": "This directory contains RDF files for each enrichment strategy. These files are combined with the base graph to create the final knowledge graph.",
+            "encodingFormat": "text/turtle",
+        },
+    )
+
+    # Add data and workflow entities:
+    base_graph_entity = crate.add_file(
+        base_graph_file,
+        properties={
+            "name": "Base WorkflowHub knowledge graph",
+            "description": "This file contains merged RDF triples from multiple RO-Crates sourced from WorkflowHub, before enrichment.",
+            "encodingFormat": "text/turtle",
+        },
+    )
+
+    full_graph_entity["isBasedOn"] = [enrichment_files, base_graph_entity]
+
+    # workflow
+    # add parameters
+    config_file_param = crate.add(
+        ContextEntity(
+            crate,
+            "#param-config",
+            properties={
+                "@type": "FormalParameter",
+                "additionalType": "File",
+                "name": "configfile",
+                "description": "A YAML file with parameter configuration for the workflow.",
+                "encodingFormat": "application/yaml",
+                "valueRequired": True,
+            },
+        )
+    )
+    config_file_param["workExample"] = config_file
+    config_file["exampleOfWork"] = config_file_param
+    full_graph_file_param = crate.add(
+        ContextEntity(
+            crate,
+            "#param-full-graph-file",
+            properties={
+                "@type": "FormalParameter",
+                "additionalType": "File",
+                "name": "Full knowledge graph RDF",
+                "description": "An RDF file containing the full knowledge graph.",
+                "encodingFormat": "text/turtle",
+            },
+        )
+    )
+    full_graph_file_param["workExample"] = full_graph_entity
+    full_graph_entity["exampleOfWork"] = full_graph_file_param
+    base_graph_file_param = crate.add(
+        ContextEntity(
+            crate,
+            "#param-base-graph-file",
+            properties={
+                "@type": "FormalParameter",
+                "additionalType": "File",
+                "name": "Base knowledge graph RDF",
+                "description": "An RDF file containing the base knowledge graph prior to enrichment.",
+                "encodingFormat": "text/turtle",
+            },
+        )
+    )
+    base_graph_file_param["workExample"] = base_graph_entity
+    base_graph_entity["exampleOfWork"] = base_graph_file_param
+    enrichment_files_param = crate.add(
+        ContextEntity(
+            crate,
+            "#param-graph-enrichment-files",
+            properties={
+                "@type": "FormalParameter",
+                "additionalType": "Dataset",
+                "name": "Enrichment outputs",
+                "description": "RDF files containing the outputs of each enrichment strategy.",
+                "encodingFormat": "text/turtle",
+            },
+        )
+    )
+    enrichment_files_param["workExample"] = enrichment_files
+    enrichment_files["exampleOfWork"] = enrichment_files_param
+    ro_crate_param = crate.add(
+        ContextEntity(
+            crate,
+            "#param-ro-crate",
+            properties={
+                "@type": "FormalParameter",
+                "additionalType": "Dataset",
+                "name": "RO-Crate output",
+                "description": "An RO-Crate containing the workflow and its outputs.",
+            },
+        )
+    )
+    enrichment_files_param["workExample"] = crate.root_dataset
+    crate.root_dataset["exampleOfWork"] = ro_crate_param
 
     workflow_entity = crate.add_workflow(
         source=workflow_file,
@@ -166,15 +285,49 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
     )
 
     workflow_entity["author"] = [auth_alex, auth_eli, auth_oliver, auth_stian]
-    workflow_entity["output"] = data_entity
+    workflow_entity["input"] = config_file_param
+    workflow_entity["output"] = [
+        full_graph_file_param,
+        base_graph_file_param,
+        enrichment_files_param,
+    ]
 
-    if "conformsTo" not in crate.root_dataset:
-        crate.root_dataset.append_to(
-            "conformsTo", {"@id": "https://w3id.org/ro/wfrun/workflow/0.5"}
+    # workflow execution action
+    execution = crate.add_action(
+        instrument=workflow_entity,
+        identifier=f"#workflow-run-{uuid.uuid4()}",
+        object=config_file,
+        result=[full_graph_entity, base_graph_entity, enrichment_files],
+        properties={"name": "Run of knowledge graph creation workflow"},
+    )
+
+    # add conforms to WRROC
+    wrroc_profile = crate.add(
+        ContextEntity(
+            crate,
+            "https://w3id.org/ro/wfrun/workflow/0.5",
+            properties={
+                "@type": "CreativeWork",
+                "name": "Workflow Run Crate",
+                "version": "0.4",
+            },
         )
+    )
+
+    crate.root_dataset.append_to("conformsTo", wrroc_profile)
 
     # Add license:
-    crate.license = "https://spdx.org/licenses/BSD-2-Clause.html"
+    license = crate.add(
+        ContextEntity(
+            crate,
+            "https://spdx.org/licenses/BSD-2-Clause.html",
+            properties={
+                "@type": "CreativeWork",
+                "name": 'BSD 2-Clause "Simplified" License',
+            },
+        )
+    )
+    crate.license = license
 
     # Writing the RO-Crate metadata:
     crate.write(output_dir)
@@ -189,7 +342,15 @@ def create_ro_crate(input_file: str, workflow_file: str, output_dir: str) -> Non
 def main():
 
     parser = argparse.ArgumentParser(description="Create RO-Crate for the RDF graph.")
-    parser.add_argument("-i", "--input-file", help="The RDF graph file.", required=True)
+    parser.add_argument(
+        "--full-graph", help="The full (enriched) RDF graph file.", required=True
+    )
+    parser.add_argument("--base-graph", help="The base RDF graph file.", required=True)
+    parser.add_argument(
+        "--enrichments-dir",
+        help="Directory containing enrichment files.",
+        required=True,
+    )
     parser.add_argument(
         "-o",
         "--output-dir",
@@ -208,12 +369,18 @@ def main():
     args = parser.parse_args()
 
     # Extract the arguments
-    input_file = args.input_file
+    full_graph_file = args.full_graph
+    base_graph_file = args.base_graph
+    enrichments_dir = args.enrichments_dir
     workflow_file = args.workflow_file
     output_dir = args.output_dir
 
     create_ro_crate(
-        input_file=input_file, workflow_file=workflow_file, output_dir=output_dir
+        full_graph_file=full_graph_file,
+        base_graph_file=base_graph_file,
+        enrichments_dir=enrichments_dir,
+        workflow_file=workflow_file,
+        output_dir=output_dir,
     )
 
 
