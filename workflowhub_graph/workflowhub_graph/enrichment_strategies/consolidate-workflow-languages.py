d6e40755737440f01e8f87ef682df78ecd3a0cef
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2025-08-22 09:06:41 +0100

    Create canonical object during enrichment

--- workflowhub_graph/workflowhub_graph/enrichment_strategies/consolidate-workflow-languages.py
+++ workflowhub_graph/workflowhub_graph/enrichment_strategies/consolidate-workflow-languages.py
@@ -1,5 +1,7 @@
 from .enrichmentABC import EnrichmentABC
 import rdflib
+from rdflib.namespace import RDF
+import requests
 
 def query_wikidata(label):
     """
@@ -11,7 +13,6 @@ def query_wikidata(label):
     Returns:
         dict: The search results from WikiData.
     """
-    import requests
 
     # Construct the API request to WikiData
     url = "https://www.wikidata.org/w/api.php"
@@ -40,6 +41,53 @@ def query_wikidata(label):
         page_id = None
     
     return page_id
+
+def create_canonical(g, wikidata_id):
+    query = f"""
+    SELECT ?wikidata_id ?wikidata_idLabel ?description ?homepage ?alias WHERE {{
+      VALUES ?wikidata_id {{ wd:{wikidata_id} }}
+      OPTIONAL {{ ?wikidata_id wdt:P856 ?homepage }}
+      OPTIONAL {{ ?wikidata_id skos:altLabel ?alias FILTER(LANG(?alias) = "en") }}
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+    }}
+    """
+
+    # Perform query
+    r = requests.get("https://query.wikidata.org/sparql",
+                     params={"query": query},
+                     headers={"Accept": "application/sparql-results+json"},
+                     timeout=10)
+    r.raise_for_status()
+
+    # Get bindings
+    bindings = r.json()["results"]["bindings"]
+
+    # Create canonical triple for the workflow language
+    SCHEMA = rdflib.Namespace("http://schema.org/")
+    wiki_uri = rdflib.URIRef(f"https://www.wikidata.org/entity/{wikidata_id}")
+    g.add((wiki_uri, RDF.type, SCHEMA.ComputerLanguage))
+
+    # Add additional metadata to node
+    print(bindings)
+    for binding in bindings:
+        if "wikidata_idLabel" in binding:
+            g.add((wiki_uri,
+                   SCHEMA.name,
+                   rdflib.Literal(binding["wikidata_idLabel"]["value"])))
+        if "description" in binding:
+            g.add((wiki_uri,
+                   SCHEMA.description,
+                   rdflib.Literal(binding["description"]["value"])))
+        if "homepage" in binding:
+            g.add((wiki_uri,
+                   SCHEMA.url,
+                   rdflib.URIRef(binding["homepage"]["value"])))
+        if "alias" in binding:
+            g.add((wiki_uri,
+                   SCHEMA.alternateName,
+                   rdflib.Literal(binding["alias"]["value"])))
+    
+    return None
 
 class ConsolidateWorkflowLanguages(EnrichmentABC):
     """
@@ -89,12 +137,14 @@ class ConsolidateWorkflowLanguages(EnrichmentABC):
                 wikidata_id = query_wikidata(name)
                 mappings[identifier] = wikidata_id
 
+                create_canonical(g, wikidata_id)
+
             # Check if we got a result from WikiData, continue if not
             if not wikidata_id:
                 print(f"No WikiData results for {name} ({identifier})")
                 continue
 
-            # Create RDF triples for the workflow language
+            # Link existing triple to the canonical object (for later consolidation)
             g.add((rdflib.URIRef(line.crate),
                    OWL.sameAs,
                    rdflib.URIRef(f"https://www.wikidata.org/entity/{wikidata_id}")))
