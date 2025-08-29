c693f4c32f7280e614ac519a33618efb9ac688bd
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2025-08-29 09:30:43 +0100

    Add test data, refactor orcid test to: test live and dead, use author in test data

--- tests/test_orcid.py
+++ tests/test_orcid.py
@@ -15,56 +15,45 @@ from workflowhub_graph.enrichment_strategies.orcid import fetch_orcid_json_data,
 @pytest.fixture(scope="module")
 def graph():
     return rdflib.Graph().parse(
-        data=open("merged.ttl", "r").read(),
+        data=open("/app/tests/test_data/test.ttl", "r").read(),
         format="turtle",
     )
 
 
-@pytest.mark.parametrize("orcid", ["https://orcid.org/0000-0001-6353-0808"])
+@pytest.mark.parametrize("orcid", ["https://orcid.org/0000-0002-7561-0810"])
 def test_workflows_of_an_author(graph, orcid):
-    # TBD: what had to be consolidated to make this work?
-    # check partial matches of ORCID names, use possible name variants?
-
-    bindings = graph.query(
-        """
-        PREFIX schema: <http://schema.org/>
-        SELECT DISTINCT ?workflow
-        WHERE {
-            ?workflow schema:author <%(orcid)s> .
-        }
-        """
-        % {"orcid": orcid}
-    ).bindings
-
-    assert len(bindings) == 3
-    assert str(sorted(bindings, key=lambda x: str(x["workflow"]))[0]["workflow"]) == "arcp://uuid,4e1e9fb8-78e1-5437-93b3-cec2ce55e5d6/"
-
-
+    # --------------------------------------------------------------------------
     # check we can fetch data from ORCID API
+    
     orcid_data = fetch_orcid_json_data(orcid.split("/")[-1])
-
     assert orcid_data is not None
-    assert orcid_data["person"]["name"]["given-names"]["value"] == "Volodymyr"
+    assert orcid_data["person"]["name"]["given-names"]["value"] == "Ivan"
+    assert orcid_data["person"]["name"]["family-name"]["value"] == "Topolsky"
 
-    # check that graph has been enriched with ORCID data
+    # --------------------------------------------------------------------------
+    # check that graph can be enriched with ORCID data
 
+    # Define query
+    query = f"""
+        PREFIX schema: <http://schema.org/> 
+        SELECT DISTINCT ?givenName ?familyName
+        WHERE {{
+            <{orcid}> schema:givenName ?givenName ;
+                        schema:familyName ?familyName .
+        }}
+        """
+
+    # Test dead
+    dead_bindings = graph.query(query).bindings
+    assert len(dead_bindings) == 0
+
+    # Add enrichment
     graph += graph_for_orcid(orcid.split("/")[-1], orcid_data)
 
-    bindings = graph.query(
-        """
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        SELECT DISTINCT ?givenName ?familyName
-        WHERE {
-            <%(orcid)s> foaf:givenName ?givenName ;
-                        foaf:familyName ?familyName .
-        }
-        """
-        % {"orcid": orcid}
-    ).bindings
-
-    assert len(bindings) == 1
-    assert str(bindings[0]["givenName"]) == "Volodymyr"
-    assert str(bindings[0]["familyName"]) == "Savchenko"
+    # Test live
+    live_bindings = graph.query(query).bindings
+    assert str(live_bindings[0]["givenName"]) == "Ivan"
+    assert str(live_bindings[0]["familyName"]) == "Topolsky"
 
 
 @pytest.mark.xfail(reason="issue #51 not yet resolved")
